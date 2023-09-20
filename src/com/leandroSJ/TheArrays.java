8f23392acf5a8f45b56975d55c3239982a3dd6da
Leandro SJ <leandro.dejesus@outlook.com.br>
2023-09-19 23:27:21 -0300

    Add New file

---
+++ src/com/leandroSJ/TheArrays.java
@@ -0,0 +1,14 @@
+package com.leandroSJ;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class TheArrays {
+    public static void main(String [] args){
+        String[] colors = new String[5];
+        colors[0] = "purple";
+        colors[1] = "blue";
+
+        System.out.println(Arrays.toString(colors));
+    }
+}
