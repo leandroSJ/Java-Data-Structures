fe5ef846f132786e0cfa157c68d2357103fc9998
Leandro SJ <leandro.dejesus@outlook.com.br>
2023-09-25 18:26:22 -0300

    Add samples

--- src/com/leandroSJ/TheArrays.java
+++ src/com/leandroSJ/TheArrays.java
@@ -1,7 +1,6 @@
 package com.leandroSJ;
-
-import java.lang.reflect.Array;
 import java.util.Arrays;
+
 
 public class TheArrays {
     public static void main(String [] args){
@@ -9,6 +8,14 @@ public class TheArrays {
         colors[0] = "purple";
         colors[1] = "blue";
 
+        System.out.println(Arrays.toString(colors));
+        System.out.println(colors[0]);
+        System.out.println(colors[1]);
+        System.out.println(colors[2]);
+        System.out.println(colors[3]);
+        System.out.println(colors[4]);
+
+        colors[2] = "yellow";
         System.out.println(Arrays.toString(colors));
     }
 }
