56ed96078d4e473ff0a869c2016030fa21b3d947
Leandro SJ <leandro.dejesus@outlook.com.br>
2023-09-26 21:09:03 -0300

    working with 2D Array

---
+++ src/com/leandroSJ/WorkingWith2DArrays.java
@@ -0,0 +1,21 @@
+package com.leandroSJ;
+
+import java.util.Arrays;
+
+import static java.util.Arrays.deepToString;
+
+public class WorkingWith2DArrays {
+    public static void main(String[] args){
+        char [][] board = new char[3][3];
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                board[i][j] = '-';
+            }
+        }
+        board[0][0] = '0';
+        board[1][0] = '0';
+        board[2][0] = '0';
+
+        System.out.println(deepToString(board));
+    }
+}
